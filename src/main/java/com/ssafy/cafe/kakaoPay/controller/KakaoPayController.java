83dd1bd552ac304e8e65c0d31c2e5aa8f9f4241c
ari <kjk9858@gmail.com>
2025-05-26 02:06:26 +0900

    결제 완료 화면 추가

--- src/main/java/com/ssafy/cafe/kakaoPay/controller/KakaoPayController.java
+++ src/main/java/com/ssafy/cafe/kakaoPay/controller/KakaoPayController.java
@@ -3,39 +3,58 @@ package com.ssafy.cafe.kakaoPay.controller;
 import com.ssafy.cafe.kakaoPay.dto.*;
 import com.ssafy.cafe.kakaoPay.service.KakaoPayService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("/rest/payment")
 @RequiredArgsConstructor
 public class KakaoPayController {
 
     private final KakaoPayService kakaoPayService;
     @PostMapping("/ready")
+    @ResponseBody
     @Operation(summary="카카오페이 요청")
     public ResponseEntity<KakaoPayReadyResponse> kakaoPayReady(@RequestBody KakaoReadyRequest requestDto) {
         KakaoPayReadyResponse response = kakaoPayService.readyToPay(requestDto);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping ("/success")
-    public ResponseEntity<KakaoApproveResponse> afterPayRequest(@RequestParam("pg_token") String pgToken) {
+//    @GetMapping ("/success")
+//    public String afterPayRequest(@RequestParam("pg_token") String pgToken, Model model) {
+//
+////        KakaoApproveResponse kakaoApprove = kakaoPayService.approveResponse(pgToken);
+//
+////        return new ResponseEntity<>(kakaoApprove, HttpStatus.OK);
+//        KakaoApproveResponse kakaoApprove = kakaoPayService.approveResponse(pgToken);
+//        model.addAttribute("approve", kakaoApprove);
+//        return "redirect:/success.html";  // resources/templates/paymentSuccess.html
+//    }
 
+    @GetMapping("/success")
+    public String afterPayRequest(@RequestParam("pg_token") String pgToken, HttpServletRequest request) {
         KakaoApproveResponse kakaoApprove = kakaoPayService.approveResponse(pgToken);
 
-        return new ResponseEntity<>(kakaoApprove, HttpStatus.OK);
+        // 결제 결과를 세션이나 쿼리 파라미터로 넘기고 HTML에서 JS로 읽을 수 있도록 할 수 있음
+        request.getSession().setAttribute("payResult", kakaoApprove);
+
+        return "redirect:/success.html";  // static 폴더의 success.html 열기
     }
 
     /**
      * 결제 진행 중 취소
      */
     @GetMapping("/cancel")
+    @ResponseBody
+
     public ResponseEntity<String> cancel() {
 
         return ResponseEntity.ok("결제취소");
@@ -46,6 +65,8 @@ public class KakaoPayController {
      */
 
     @GetMapping("/fail")
+    @ResponseBody
+
     public ResponseEntity<String> fail() {
 
         return ResponseEntity.ok("결제오류");
@@ -54,6 +75,8 @@ public class KakaoPayController {
 
     //결제 내역 조회
     @PostMapping("/order")
+    @ResponseBody
+
     public ResponseEntity<Boolean> order(@RequestBody KakaoPaymentOrderRequest request){
 
         KakaoPaymentOrderResponse orderResponse=kakaoPayService.orderResponse(request);
